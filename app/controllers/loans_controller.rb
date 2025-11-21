ee48f5d9d5c00b584636c8a8c70ba43d31ae6f69
chris-clifford <cclifford82@gmail.com>
2025-11-21 11:26:06 -0800

    move csv parsing to loan model

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -27,31 +27,10 @@ class LoansController < ApplicationController
     csv_source = file.respond_to?(:tempfile) ? file.tempfile : file
 
     CSV.foreach(csv_source, headers: true) do |row|
-      Loan.create!(attributes_from(row))
+      Loan.create_from_csv_row!(row)
       imported_count += 1
     end
 
     imported_count
-  end
-
-  def attributes_from(row)
-    {
-      loan_number: row["loan_number"]&.strip,
-      origination_date: parse_date(row["origination_date"]),
-      amortization_period: row["amortization_period"].presence&.to_i,
-      payment_date: parse_date(row["payment_date"]),
-      unpaid_principal_balance: row["unpaid_principal_balance"].presence&.to_d,
-      interest_rate: row["interest_rate"].presence&.to_d,
-      net_operating_income: row["net_operating_income"].presence&.to_d,
-      is_interest_only: ActiveModel::Type::Boolean.new.cast(row["is_interest_only"])
-    }
-  end
-
-  def parse_date(value)
-    return if value.blank?
-
-    Date.strptime(value, "%m/%d/%Y")
-  rescue ArgumentError
-    nil
   end
 end
--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,3 +1,32 @@
 class Loan < ApplicationRecord
   validates :loan_number, :unpaid_principal_balance, :interest_rate, presence: true
+
+  def self.create_from_csv_row!(row)
+    create!(attributes_from(row))
+  end
+
+  private
+
+  def self.attributes_from(row)
+    {
+      loan_number: row["loan_number"]&.strip,
+      origination_date: parse_date(row["origination_date"]),
+      amortization_period: row["amortization_period"].presence&.to_i,
+      payment_date: parse_date(row["payment_date"]),
+      unpaid_principal_balance: row["unpaid_principal_balance"].presence&.to_d,
+      interest_rate: row["interest_rate"].presence&.to_d,
+      net_operating_income: row["net_operating_income"].presence&.to_d,
+      is_interest_only: ActiveModel::Type::Boolean.new.cast(row["is_interest_only"])
+    }
+  end
+
+  def self.parse_date(value)
+    return if value.blank?
+
+    Date.strptime(value, "%m/%d/%Y")
+  rescue ArgumentError
+    nil
+  end
+
+  private_class_method :attributes_from, :parse_date
 end
